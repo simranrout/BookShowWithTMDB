c5acd9af1bcc6ef0b01f495658e9e14b1c2e8359
Simran Rout <simranrout17@gmail.com>
2021-08-20 20:05:09 +0530

    updated MovieTableViewController

--- BookShowWithTMDB/Controllers/Movie Page Controller/MovieTabViewController.swift
+++ BookShowWithTMDB/Controllers/Movie Page Controller/MovieTabViewController.swift
@@ -28,53 +28,35 @@ class MovieTabViewController: UIViewController , UITableViewDelegate , UITableVi
     
     // Mark:- Conforms to APICallDelegate to fetch the movie data in background and when the data ready then reload the table view
     func resultFetched(_ MoviesData: [MovieModel]) {
-        self.Results = MoviesData
+            self.Results = MoviesData
         
-        //refresh the tableview with new data
-       
+            //refresh the tableview with new data
             self.TableView.reloadData()
-     
-        
     }
     
+    //Passing the data (Movie Model for specific row ) to Movie Details View controller through segue
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         
           guard TableView.indexPathForSelectedRow != nil else {
               return
           }
-        
         let updatedResult = Results[TableView.indexPathForSelectedRow!.row]
         let MovieDetailsVC =  segue.destination as! MovieDetailsViewController
         MovieDetailsVC.updateMovieDetails = updatedResult
-    
-        
     }
+    
     // MARK :- TableView Methods
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        
         return Results.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = TableView.dequeueReusableCell(withIdentifier: Constants.MoviePrototypeCellID, for: indexPath) as! MovieTableViewCell
-        
         cell.displayingDataOnCell(self.Results[indexPath.row])
-        
         //return cell
         return cell
         
     }
-    
-    /*
-     Tableview will call this function when user taps on a row
-     */
-//    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-//
-//
-//    }
-//
-    
-
 }
 
 
